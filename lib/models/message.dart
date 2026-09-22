6f59a2847651f540624c8af8f083c1535b8675e2
Simon tech <officialsimondon@gmail.com>
2026-09-22 11:31:57 +0100

    Add Message class with JSON serialization methods

---
+++ lib/models/message.dart
@@ -0,0 +1,30 @@
+class Message {
+  final String text;
+  final bool isUser;
+  final DateTime timestamp;
+
+  Message({
+    required this.text,
+    required this.isUser,
+    DateTime? timestamp,
+  }) : timestamp = timestamp ?? DateTime.now();
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'isUser': isUser,
+      'timestamp': timestamp.toIso8601String(),
+    };
+  }
+
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      text: json['text'] ?? '',
+      isUser: json['isUser'] ?? false,
+      timestamp: DateTime.tryParse(
+            json['timestamp'] ?? '',
+          ) ??
+          DateTime.now(),
+    );
+  }
+}
